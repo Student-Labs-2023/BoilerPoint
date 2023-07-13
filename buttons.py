e1699859996936dc14e5a24795dd8aadf68b79cb
Qubicool <90842082+Qubicool@users.noreply.github.com>
2023-07-13 21:45:37 +0600

    Features (sprint 2)
    - Реализована основная структура бота через FSM(Finite State Machine)
    - Реализована регистрация пользователя с использованием базы данных YDB 
    - Добавлена часть кнопок
    - Реализован каркас валидации данных

---
+++ buttons.py
@@ -0,0 +1,31 @@
+from aiogram.types import InlineKeyboardMarkup,InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
+#gender buttons
+
+ikbg = InlineKeyboardMarkup(row_width=2)
+ibm = InlineKeyboardButton(text="Мужчина",callback_data='Male')
+ibf = InlineKeyboardButton(text="Женщина",callback_data='Female')
+ikbg.add(ibm,ibf)
+
+#menu buttons
+rkbm =  ReplyKeyboardMarkup(resize_keyboard=True)
+kprofile= KeyboardButton(text="👤Профиль")
+kliderboard= KeyboardButton(text="📊Рейтинг")
+kschedule= KeyboardButton(text="📆Календарь событий")
+khelp= KeyboardButton(text="❓Помощь")
+kexercise= KeyboardButton(text="📝Задания")
+rkbm.row(kprofile,kexercise)
+rkbm.add(kschedule)
+rkbm.row(khelp,kliderboard)
+
+#admin buttons
+admin_list=['5617565289']
+admrkbm = ReplyKeyboardMarkup(resize_keyboard=True)
+admk_user_editor = KeyboardButton(text="⚙️Изменить пользователя")
+admk_event_creation = KeyboardButton(text="🛠️Создать мероприятие")
+admk_job_creation = KeyboardButton(text="📝Создать задание")
+admk_menu = KeyboardButton(text="⬅️Меню")
+admk_liderboard = KeyboardButton(text="📊Рейтинг")
+admrkbm.row(admk_user_editor)
+admrkbm.row(admk_event_creation)
+admrkbm.row(admk_job_creation)
+admrkbm.row(admk_menu,admk_liderboard)
