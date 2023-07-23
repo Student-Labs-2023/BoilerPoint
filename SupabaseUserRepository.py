474a9dcb3715fed7f2b2b67640bb67395c213a9b
Qubicool <90842082+Qubicool@users.noreply.github.com>
2023-07-23 18:09:12 +0600

    Features-B4-MakePatternRepo
    - Replace some func in main with user.get and user.set
    - Change SupabaseUserRepository. Functions: get,set now can handle tgusr by ValueError

---
+++ SupabaseUserRepository.py
@@ -0,0 +1,72 @@
+from .usersrepository import UserRepository
+from typing import List
+from src.models.users import User
+from supabase import Client, create_client
+
+
+
+class SupabaseUserRepository(UserRepository):
+    
+    def __init__(self,client:Client, table_name:str = 'UsersData'):
+        self.client = client
+        self.table_name = table_name
+
+    def get(self,id : str) -> User:
+        try:
+            chat_id:int = int(id)
+            response = self.client.table(self.table_name).select('full_name','gender','age','balance','tgusr','user_state').eq('chat_id', chat_id).execute()
+            item = response.data[0]
+            user_data = item
+            pseudo = user_data.get('full_name', 'Unknown')
+            gender = user_data.get('gender', 'Unknown')
+            age = user_data.get('age', 'Unknown')
+            balance = user_data.get('balance')
+            user_state = user_data.get('user_state')
+            tgusr = user_data.get('tgusr')
+            return User(
+                chat_id=chat_id,
+                full_name=pseudo,
+                gender=gender,
+                user_state=user_state,
+                age=age,
+                balance=balance,
+                tgusr = tgusr
+            )
+        except ValueError:
+            tgusr:str = id
+            response = self.client.table(self.table_name).select('full_name','gender','age','balance','chat_id','user_state').eq('tgusr', tgusr).execute()
+            item = response.data[0]
+            user_data = item
+            chat_id = user_data.get('chat_id')
+            pseudo = user_data.get('full_name', 'Unknown')
+            gender = user_data.get('gender', 'Unknown')
+            age = user_data.get('age', 'Unknown')
+            balance = user_data.get('balance')
+            user_state = user_data.get('user_state')
+            return User(
+                chat_id = chat_id,
+                full_name=pseudo,
+                gender=gender,
+                user_state=user_state,
+                age=age,
+                balance=balance,
+                tgusr = tgusr
+            )        
+        
+    def set(self,user : User) -> None:
+        if self.get(user.chat_id):  
+            self.client.table(self.table_name).update(user.dict()).eq('chat_id',user.chat_id).execute()
+        else:
+            #insert
+            self.client.table(self.table_name).insert(user.dict()).execute()
+    
+    def delete(self,user : User) -> None:
+        try:
+            if self.get(user.chat_id):
+                self.client.table(self.table_name).delete().eq('chat_id',user.chat_id).execute()
+        except Exception as e:
+            print(f"Error delete user {user.chat_id} error : {e}")
+
+            
+    def list(self,**kwargs) -> List[User]: 
+        ...
