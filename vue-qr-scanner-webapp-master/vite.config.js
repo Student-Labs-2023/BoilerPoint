a1a4a2bf8e1c1742439fe08172107fb4b4d38cc1
Данил <128917932+Goldenbarnowl@users.noreply.github.com>
2023-08-09 20:52:37 +0600

    Features-A6-QRWebApp
    - веб приложение для скана QR кодов

---
+++ vue-qr-scanner-webapp-master/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vuetify()
+  ],
+  server: {
+    port: 3001
+  }
+})
